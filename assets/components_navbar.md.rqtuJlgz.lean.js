79f843ee205403ac7996a66bf5a3903b32548d7c
DanKaufmanDev <daniel.kaufman_1@yahoo.ca>
2026-06-01 14:54:15 -0400

    deploy docs

---
+++ assets/components_navbar.md.rqtuJlgz.lean.js
@@ -0,0 +1,13 @@
+import{_ as a,I as n,c as t,o as r,j as e,a as i,J as l}from"./chunks/framework.DuYXm8Z9.js";const x=JSON.parse('{"title":"Navbar","description":"","frontmatter":{"aside":false},"headers":[],"relativePath":"components/navbar.md","filePath":"components/navbar.md"}'),c={name:"components/navbar.md"};function m(p,o,d,b,v,u){const s=n("ComponentPreview");return r(),t("div",null,[o[0]||(o[0]=e("h1",{id:"navbar",tabindex:"-1"},[i("Navbar "),e("a",{class:"header-anchor",href:"#navbar","aria-label":"Permalink to “Navbar”"},"​")],-1)),o[1]||(o[1]=e("div",{class:"important custom-block github-alert"},[e("p",{class:"custom-block-title"},"IMPORTANT"),e("p",null,"IconForge must be installed for the components to render.")],-1)),e("div",null,[l(s,{snippet:`
+<nav class='is-nav is-bg-coal-7 is-color-coal-10 is-px-[1.5rem] is-py-[0.75rem] is-flex is-justify-between is-items-center is-gap-[1rem]'>
+  <a class='is-size-lg is-font-bold'>IconForge</a>
+  <div class='is-flex is-gap-[1rem]'>
+    <a class='is-color-coal-10 is-text-none hover:is-color-blue-6'>Home</a>
+    <a class='is-color-coal-10 is-text-none hover:is-color-blue-6'>Docs</a>
+    <a class='is-color-coal-10 is-text-none hover:is-color-blue-6'>Playground</a>
+  </div>
+  <button class='is-px-[0.75rem] is-py-[0.4rem] is-bg-blue-6 hover:is-bg-blue-7 is-color-white is-rounded-sm'>
+    Get Started
+  </button>
+</nav>
+`})])])}const g=a(c,[["render",m]]);export{x as __pageData,g as default};
