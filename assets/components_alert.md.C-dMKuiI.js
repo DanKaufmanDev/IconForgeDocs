070df30bac093a84d40b645a3edc6b4bb7e1306b
DanKaufmanDev <daniel.kaufman_1@yahoo.ca>
2026-06-01 15:21:09 -0400

    deploy docs

--- assets/components_alert.md.C-dMKuiI.js
+++
@@ -1,6 +0,0 @@
-import{_ as a,I as o,c as r,o as n,j as e,a as i,J as l}from"./chunks/framework.DuYXm8Z9.js";const w=JSON.parse('{"title":"Alerts","description":"","frontmatter":{"aside":false},"headers":[],"relativePath":"components/alert.md","filePath":"components/alert.md"}'),c={name:"components/alert.md"},d={style:{display:"flex","flex-direction":"row","flex-wrap":"wrap",gap:"1rem"}};function p(m,s,f,u,v,_){const t=o("ComponentPreview");return n(),r("div",null,[s[0]||(s[0]=e("h1",{id:"alerts",tabindex:"-1"},[i("Alerts "),e("a",{class:"header-anchor",href:"#alerts","aria-label":"Permalink to “Alerts”"},"​")],-1)),s[1]||(s[1]=e("div",{class:"important custom-block github-alert"},[e("p",{class:"custom-block-title"},"IMPORTANT"),e("p",null,"IconForge must be installed for the components to render.")],-1)),e("div",d,[l(t,{snippet:`
-<div class='is-rounded-sm is-bg-yellow-4 is-color-yellow-10 is-p-[1rem] is-inline-flex is-items-start is-gap-[0.5rem]'>
-  <i class='if-warningtri is-size-[1.35rem]'></i>
-  <div><strong class='is-block'>Heads up!</strong>This is a warning alert.</div>
-</div>
-`})])])}const x=a(c,[["render",p]]);export{w as __pageData,x as default};
